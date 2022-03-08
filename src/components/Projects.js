920bf53494e4b568b9e6442c02f16d508fe12f6f
CodyConck <codyconck@gmail.com>
2022-03-08 11:55:56 -0600

    added images to project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,13 @@ import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
+import collage from '../assets/CollageSoA.png'
+import bar from '../assets/bar.png'
+import print from '../assets/print.png'
+import task from '../assets/task.png'
+import team from '../assets/team.png'
+import regex from '../assets/regex.png'
+
 import React from 'react'
 
 function Projects() {
@@ -11,49 +18,42 @@ function Projects() {
         {
             name: 'State of the Art',
             description: 'This full stack web application allows users to search for and locate works of art.',
-            img: (''),
+            img: collage,
             github: 'https://github.com/CodyConck/ArtFiles2',
             deployed: 'https://state-of-art.herokuapp.com/'
         },
         {
-            name: 'Brewed Force',
-            description: 'A website designed for globe-trotting beer enthusiasts that matches users with breweries in new cities.',
-            img: (''),
-            github: 'https://github.com/CodyConck/globe-trotting-and-bar-hopping',
-            deployed: 'https://takes101.github.io/globe-trotting-and-bar-hopping/'
-        },
-        {
             name: 'One-Stop-Print-Shop',
             description: 'Full stack MERN application that serves as an e-commercer site for a print shop.',
-            img: (''),
+            img: print,
             github: 'https://github.com/CodyConck/One-Stop-Print-Shop',
             deployed: 'https://one-stop-print-shop.herokuapp.com/'
         },
         {
-            name: 'React-Task-Tracker2021',
-            description: 'A simple React based task organizer, built as an exercise to better understand the React framework. This was based off of a YouTube tutorial from Traversy Media',
-            img: (''),
-            github: 'https://github.com/CodyConck/React-Task-Tracker2021',
-            deployed: 'https://codyconck.github.io/React-Task-Tracker2021/'
-        },
-        {
-            name: 'File-Uploader',
-            description: 'Front End of a file uploader built with react. Clone repo, install necessary dependencies, run "npm run dev"!',
-            img: (''),
-            github: 'https://github.com/CodyConck/file-uploader',
-            deployed: ''
-        },
-        {
             name: 'Team Profile Generator',
             description: 'Command line application that uses Node.js to prompt a user to enter in information about their work team members. Once all prompts are answered, an HTML is generated using the user input to populate cards for each employees position.',
-            img: (''),
+            img: team,
             github: 'https://github.com/CodyConck/Team-Profile-Generator',
             deployed: 'https://drive.google.com/file/d/1OTRL2HgAlR27h5GKXHAFKVOf1DqCVLPD/view'
         },
         {
+            name: 'Brewed Force',
+            description: 'A website designed for globe-trotting beer enthusiasts that matches users with breweries in new cities.',
+            img: bar,
+            github: 'https://github.com/CodyConck/globe-trotting-and-bar-hopping',
+            deployed: 'https://takes101.github.io/globe-trotting-and-bar-hopping/'
+        },
+        {
+            name: 'React-Task-Tracker2021',
+            description: 'A simple React based task organizer, built as an exercise to better understand the React framework. This was based off of a YouTube tutorial from Traversy Media',
+            img: task,
+            github: 'https://github.com/CodyConck/React-Task-Tracker2021',
+            deployed: 'https://codyconck.github.io/React-Task-Tracker2021/'
+        },
+        {
             name: 'Regex Tutorial',
             description: 'In this tutorial, I will break down a Regular Expression/Regex that is used to match an email.',
-            img: (''),
+            img: regex,
             github: 'https://gist.github.com/CodyConck/1798fe20b62c67902415ea64e055943c',
             deployed: ''
         }
